50343e58c9074dab3296760d485486d7b5b9bd7f
avgamecompany <originalavgamecompany@gmail.com>
2024-08-14 16:21:59 +0300

    Add files via upload

---
+++ play.js
@@ -0,0 +1,11 @@
+function prime(x) {
+
+    var d= document.createElement("div");
+
+
+    d.innerText =x
+
+    document.getElementById("x").append(d)
+}
+
+prime("hello my name is aviv lazarovich and i am the creator of the avgame company that i belive to be the goodest gaming company and with all of that it have a site here is it "); 
